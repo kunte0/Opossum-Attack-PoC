30672e7a5556d06544a784f32423dcef656f3675
kunte_ <lukas.knittel@rub.de>
2025-07-28 18:35:25 +0200

    fixed #1 by downgrading to ubuntu20.04

---
+++ servers/apache2/certs/create_certs.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# create self signed certificate
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+  -keyout server.key \
+  -addext "basicConstraints=CA:FALSE" \
+  -out server.crt \
+  -subj "/CN=localhost"
--- servers/apache2/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Start cupsd
-apache2ctl -D FOREGROUND &
-
-# error log
-touch /var/log/apache2/error.log
-tail -f /var/log/apache2/error.log
